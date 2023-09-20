5d47a5ad95f64b26210ddee59aef7d66fe02261e
PJins <53553045+PJins@users.noreply.github.com>
2023-09-20 21:21:25 +0900

    Add files via upload

---
+++ arrange.sh
@@ -0,0 +1,185 @@
+shopt -s globstar
+
+dirPath="files"
+
+#a
+for file in $dirPath/[aA]*; do
+    if [ -e $file ]; then
+        mv $file a/
+    fi
+done
+
+#b
+for file in $dirPath/[bB]*; do
+    if [ -e $file ]; then
+        mv $file b/
+    fi
+done
+
+#c
+for file in $dirPath/[cC]*; do
+    if [ -e $file ]; then
+        mv $file c/
+    fi
+done
+
+#d
+for file in $dirPath/[dD]*; do
+    if [ -e $file ]; then
+        mv $file d/
+    fi
+done
+
+#e
+for file in $dirPath/[eE]*; do
+    if [ -e $file ]; then
+        mv $file e/
+    fi
+done
+
+#f
+for file in $dirPath/[fF]*; do
+    if [ -e $file ]; then
+        mv $file F/
+    fi
+done
+
+#g
+for file in $dirPath/[gG]*; do
+    if [ -e $file ]; then
+        mv $file g/
+    fi
+done
+
+#h
+for file in $dirPath/[hH]*; do
+    if [ -e $file ]; then
+        mv $file h/
+    fi
+done
+
+#i
+for file in $dirPath/[iI]*; do
+    if [ -e $file ]; then
+        mv $file i/
+    fi
+done
+
+#j
+for file in $dirPath/[jJ]*; do
+    if [ -e $file ]; then
+        mv $file j/
+    fi
+done
+
+#k
+for file in $dirPath/[kK]*; do
+    if [ -e $file ]; then
+        mv $file k/
+    fi
+done
+
+#l
+for file in $dirPath/[lL]*; do
+    if [ -e $file ]; then
+        mv $file l/
+    fi
+done
+
+#m
+for file in $dirPath/[mM]*; do
+    if [ -e $file ]; then
+        mv $file m/
+    fi
+done
+
+#n
+for file in $dirPath/[nN]*; do
+    if [ -e $file ]; then
+        mv $file n/
+    fi
+done
+
+#o
+for file in $dirPath/[oO]*; do
+    if [ -e $file ]; then
+        mv $file o/
+    fi
+done
+
+#p
+for file in $dirPath/[pP]*; do
+    if [ -e $file ]; then
+        mv $file p/
+    fi
+done
+
+#q
+for file in $dirPath/[qQ]*; do
+    if [ -e $file ]; then
+        mv $file q/
+    fi
+done
+
+#r
+for file in $dirPath/[rR]*; do
+    if [ -e $file ]; then
+        mv $file r/
+    fi
+done
+
+#s
+for file in $dirPath/[sS]*; do
+    if [ -e $file ]; then
+        mv $file s/
+    fi
+done
+
+#t
+for file in $dirPath/[tT]*; do
+    if [ -e $file ]; then
+        mv $file t/
+    fi
+done
+
+#u
+for file in $dirPath/[uU]*; do
+    if [ -e $file ]; then
+        mv $file u/
+    fi
+done
+
+#v
+for file in $dirPath/[vV]*; do
+    if [ -e $file ]; then
+        mv $file v/
+    fi
+done
+
+#w
+for file in $dirPath/[wW]*; do
+    if [ -e $file ]; then
+        mv $file w/
+    fi
+done
+
+#x
+for file in $dirPath/[xX]*; do
+    if [ -e $file ]; then
+        mv $file x/
+    fi
+done
+
+#y
+for file in $dirPath/[yY]*; do
+    if [ -e $file ]; then
+        mv $file y/
+    fi
+done
+
+#z
+for file in $dirPath/[zZ]*; do
+    if [ -e $file ]; then
+        mv $file z/
+    fi
+done
